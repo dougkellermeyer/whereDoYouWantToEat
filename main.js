ff6ab76b535863befc607a66ca3d122cdf073995
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2020-02-16 17:40:07 -0500

    hooked up filters

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const restaurants = [
     {name: 'Trata', category: "American"},
     {name: 'Roux', category: "French"},
-    {name: 'Owl House', category: ""},
+    {name: 'Owl House', category: "American"},
     {name: 'Texas Roadhouse', category: "American"},
     {name: 'Hong Wah', category: "Asian"},
     {name: 'Roam', category: "American"},
@@ -9,10 +9,10 @@ const restaurants = [
     {name: "McDonald's", category: "American"},
     {name: "Tom Wahl's", category: "American"},
     {name: "Bill Gray's", category: "American"},
-    {name:  "Wegman's", category: ""},
+    {name:  "Wegman's", category: "Wegman's"},
     {name: "P.F. Chang's", category: "Asian"},
     {name: 'Cheesecake Factory', category: "American"},
-    {name:"Moe's", category: "Mexican"},
+    {name: "Moe's", category: "Mexican"},
     {name: 'Revelry', category: "American"},
     {name: 'Food at Fisher Station', category: "Breakfast"},
     {name: 'Char Broil', category: "Breakfast"},
@@ -23,30 +23,30 @@ const restaurants = [
     {name: 'Panera', category: "Sandwiches"},
     {name: 'Fiamma Centro', category: "Italian"},
     {name: 'Nosh', category: "American"},
-    {name:  'Philly Steak Out',category: "American"},
-
-    
-    "Pontillo's",
-    "Mr. Dominic's",
-    "Joey's",
-    "Michael's Valley Grill",
-    "Rosey's",
-    "Good Luck",
-    'Old Stone Tavern',
-    "Gate House",
-    "Pane Vino",
-    "Thali of India",
-    "Branca",
-    "Max of Eastman Place",
-    "Dogtown",
-    "McCann's Meats",
-    "Bitter Honey",
-    "Pizza Stop",
-    "Phillips European",
-    "DiBella's"
+    {name: 'Philly Steak Out',category: "American"},
+    {name: "Pontillo's", category: "Pizza"},
+    {name: "Mr. Dominic's", category: "Italian"},
+    {name: "Joey's", category: "Italian"},
+    {name: "Michael's Valley Grill", category: "Italian"},
+    {name: "Marks", category: "Pizza"},
+    {name: "Rosey's", category: "Italian"},
+    {name: "Good Luck", category: "American"},
+    {name: 'Philly Steak Out', category: "American"},
+    {name: 'Old Stone Tavern', category: "American"},
+    {name: "Gate House", category: "American"},
+    {name: "Pane Vino", category: "Italian"},
+    {name: "Thali of India", category: "Asian"},
+    {name: "Branca", category: "Italian"},
+    {name: "Max of Eastman Place", category: "Italian"},
+    {name: "Dogtown", category: "American"},
+    {name: "McCann's Meats", category: "American"},
+    {name: "Bitter Honey", category: "Mexican"},
+    {name: "Pizza Stop", category: "Pizza"},
+    {name: "Phillips European", category: "Italian"},
+    {name: "DiBella's", category: "Sandwiches"}
 ];
 
-const categories = ["Italian", "American", "Fast-food", "Sandwiches", "Bakeries", "Asian", "Breakfast", "French", "Mexican", "Greek"];
+const categories = ["Italian", "American", "Fast-food", "Sandwiches", "Bakeries", "Asian", "Breakfast", "French", "Mexican", "Greek", "Pizza"];
 
 const giveRandomRestaurant = () => {
     const anyRestaurant = document.getElementById("anyRestaurant");
@@ -63,8 +63,11 @@ const generateDropdown = () =>  {
 const categoryList = document.getElementById("categoryList");
 
 const showCategoryList = (category) => {
-    categoryList.innerHTML = "<h3>" + category + " restaurants:" + "</h3>"; 
+        categoryList.innerHTML = 
+    "<h3>" + category + " restaurants:" + "</h3>" + 
+    "<ul>" + restaurants.filter(restaurant => restaurant.category === category).map(match => "<li>" + match.name + "</li>").join('') + "</ul>";
 };
+
 
 generateDropdown();
 
