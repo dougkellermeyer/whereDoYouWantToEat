378ab3383d74318e7cb61c6ac1c1c8efb7efa1cb
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2020-02-16 14:28:20 -0500

    added category dropdown

--- main.js
+++ main.js
@@ -4,30 +4,19 @@ var restaurants = [
     'Owl House'
 ];
 
-var categories = ["Category 1", "Category 2", "Category 3"];
+var categories = ["Italian", "American", "Fast Food", "Sandwiches", "Bakeries", "Asian", "Breakfast", ""];
 var descriptions = ["Description 1", "Description 2", "Description 3"];
 
-function generateHTML() {
-    var categoryWrapper = document.getElementById("categoryWrapper");
-    categoryWrapper.innerHTML = 
-            "<div class='col-sm-6 col-md-4'>" +
-    "<div class='caption'>" +
-        "<img src='...' alt='...'>" +
-        "<div class='caption'>" +
-        //map through each resturant, description and category
-            "<h3>" + categories + "</h3>" +
-            "<p>" + descriptions + "</p>" +
-            "<p>" +
-                "<a href='#' class='btn btn-primary' role='button'>" + "</a>" +
-                "<a href='#' class='btn btn-default' role='button'>" + "</a>"+
-            "</p>" +
-        "</div>" +
-   " </div>" +
-"</div>"
-    ;
+function generateDropdown() {
+    var categoryDropdown = document.getElementById("categoryDropdown");
+    categoryDropdown.insertAdjacentHTML("afterbegin",
+        categories.map(category => {
+           return "<a class='dropdown-item'>" + category + "</a>";
+        }).join('')
+    );
 };
 
 
-generateHTML();
+generateDropdown();
 
 
